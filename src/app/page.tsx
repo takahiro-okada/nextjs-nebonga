5d13dc325374e6e161c80a6f4910874b4eea02fa
takahiro-okada <okahanako09875@gmail.com>
2023-10-20 05:19:08 +0900

    update:Using useState and useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,62 @@
+'use client'
+import { useState, useEffect } from 'react'
 import Header from './Header'
 
 type Post = {
   node: { title: string; id: string; content: string }
 }
 
-export default async function Home() {
-  const query = `query NewQuery {
-    posts {
-      edges {
-        node {
-          title
-          id
-          content
+export default function Home() {
+  const [posts, setPosts] = useState<Post[] | null>(null)
+
+  useEffect(() => {
+    const query = `query NewQuery {
+      posts {
+        edges {
+          node {
+            title
+            id
+            content
+          }
         }
       }
-    }
-  }`
+    }`
 
-  const posts = await fetch('https://hirokawasaki-works.com/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ query }),
-    next: { revalidate: 10 },
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('HTTPエラー ' + res.status)
-      }
-      return res.json()
+    fetch('https://hirokawasaki-works.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
     })
-    .then((json) => {
-      return json.data.posts.edges
-    })
-    .catch((error) => {
-      console.error('エラー: ' + error.message)
-    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('HTTPエラー ' + res.status)
+        }
+        return res.json()
+      })
+      .then((json) => {
+        setPosts(json.data.posts.edges)
+      })
+      .catch((error) => {
+        console.error('エラー: ' + error.message)
+      })
+  }, [])
+
+  if (!posts) {
+    return <div>Loading...</div>
+  }
 
   return (
     <main>
       <Header />
-      {posts.map((post: Post, i: number) => {
-        return (
-          <div key={i}>
-            <div>{post.node.title}</div>
-            <div>{post.node.id}</div>
-            <div dangerouslySetInnerHTML={{ __html: post.node.content }}></div>
-          </div>
-        )
-      })}
+      {posts.map((post, i) => (
+        <div key={i}>
+          <div>{post.node.title}</div>
+          <div>{post.node.id}</div>
+          <div dangerouslySetInnerHTML={{ __html: post.node.content }}></div>
+        </div>
+      ))}
     </main>
   )
 }
