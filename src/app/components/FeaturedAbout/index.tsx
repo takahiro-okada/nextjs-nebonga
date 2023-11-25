bb728fac1b37e876d70a5e4769d06c5202015165
takahiro-okada <okahanako09875@gmail.com>
2023-11-26 05:46:12 +0900

    update: FeaturedAbout section

--- src/app/components/FeaturedAbout/index.tsx
+++ src/app/components/FeaturedAbout/index.tsx
@@ -1,42 +1,64 @@
 import Image from 'next/image'
 
+import { SectionTitle } from '../SectionTitle'
+
 export function FeaturedAbout() {
   return (
-    //tailwindcssで2カラムレイアウトを作成をして左側に画像､右側にテキストを配置
     <section className='mt-32'>
-      <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
-        <div className='relative'>
-          <Image src='/images/bg-sample.jpg' alt='featured-about' objectFit='cover' width={640} height={480} />
-        </div>
-        <div className='flex flex-col items-start justify-center'>
-          <h3 className='text-3xl font-bold'>About</h3>
-          <p className='mt-4'>
-            はじめまして､nebongaと申します｡
-            <br />
-            このサイトでは､主にフロントエンド開発についての記事を書いていきます｡
-            <br />
-            また､日々の学びや､制作物についても紹介していきます｡
-            <br />
-            どうぞよろしくお願いいたします｡
-          </p>
-        </div>
-      </div>
+      <div className='container mx-auto px-3'>
+        <SectionTitle title='NeBongaについて' subtitle='About Us' />
+        <div className='mt-16'>
+          <div className='gap-10 md:flex md:flex-row-reverse'>
+            <div className='relative md:w-1/2'>
+              <Image
+                src='/images/bg-sample.jpg'
+                alt='featured-about'
+                objectFit='cover'
+                width={640}
+                height={480}
+                className='rounded-xl'
+              />
+            </div>
+            <div className='flex flex-col items-start justify-center md:w-1/2'>
+              <h3 className='mt-3 text-2xl'>”先導する”ブランディング</h3>
+              <p className='text-sm'>Lead your brand</p>
 
-      <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
-        <div className='flex flex-col items-start justify-center'>
-          <h3 className='text-3xl font-bold'>About</h3>
-          <p className='mt-4'>
-            はじめまして､nebongaと申します｡
-            <br />
-            このサイトでは､主にフロントエンド開発についての記事を書いていきます｡
-            <br />
-            また､日々の学びや､制作物についても紹介していきます｡
-            <br />
-            どうぞよろしくお願いいたします｡
-          </p>
-        </div>
-        <div className='relative'>
-          <Image src='/images/bg-sample.jpg' alt='featured-about' objectFit='cover' width={640} height={480} />
+              <p className='mt-4'>
+                写真や映像で人の魅力を引き出し、ストーリーを伝えます。撮影に入るまでの対話を大切にすることを心がけ、「半歩先を歩く黒子」を常に目指しています。
+              </p>
+              <p className='mt-4'>
+                I capture the essence and charm of individuals through photos and videos, conveying their stories. I
+                always prioritize meaningful dialogue before any shoot, and constantly strive to be the unseen guide,
+                always a step ahead.
+              </p>
+            </div>
+          </div>
+
+          <div className='mt-8 gap-8 md:flex'>
+            <div className='relative md:w-1/2'>
+              <Image
+                src='/images/bg-sample.jpg'
+                alt='featured-about'
+                objectFit='cover'
+                width={640}
+                height={480}
+                className='rounded-xl'
+              />
+            </div>
+            <div className='flex flex-col items-start justify-center md:w-1/2'>
+              <h3 className='mt-3 text-2xl'>”先導する”ブランディング</h3>
+              <p className='text-sm'>Lead your brand</p>
+
+              <p className='mt-4'>
+                写真や映像で人の魅力を引き出し、ストーリーを伝えます。撮影に入るまでの対話を大切にすることを心がけ、「半歩先を歩く黒子」を常に目指しています。
+              </p>
+              <p className='mt-4'>
+                I capture the essence and charm of individuals through photos and videos, conveying their stories. I
+                always prioritize meaningful dialogue before any shoot, and constantly strive to be the unseen guide,
+                always a step ahead.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
