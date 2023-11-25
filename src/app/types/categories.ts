aea1233ecf1428ca03945adb28ca7880a82ee27b
takahiro-okada <okahanako09875@gmail.com>
2023-11-26 06:39:05 +0900

    update:Modified Category type

--- src/app/types/categories.ts
+++ src/app/types/categories.ts
@@ -1,6 +1,22 @@
-type Categories = {
-  id: number
+interface Node {
+  id: string
   name: string
+  children: Children
+  uri: string // Node は Children 型の children プロパティを持つ
+}
+
+interface Edge {
+  node: Node
+}
+
+interface Children {
+  edges: Edge[]
+}
+
+interface Categories {
+  id: string // JSONデータに基づいて、idは文字列である必要があります
+  name: string
+  children: Children
   uri: string
 }
 
