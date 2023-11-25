3246f13acda30f3499b882b247556157c8e96460
takahiro-okada <okahanako09875@gmail.com>
2023-11-26 05:45:46 +0900

    update: Update SectionTitle component

--- src/app/components/SectionTitle/index.tsx
+++ src/app/components/SectionTitle/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export function SectionTitle({ title, subtitle }: Props) {
   return (
-    <section className='mt-16 flex items-center'>
+    <section className='flex items-center'>
       <h2 className='text-3xl'>{title}</h2>
       <span className='ml-5 text-xs'>ー {subtitle}</span>
     </section>
