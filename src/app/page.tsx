4b7e4a54f529e6da2e8479179b6fa656e2073be4
takahiro-okada <okahanako09875@gmail.com>
2024-03-16 05:47:25 +0900

    update: add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import CardImage from './components/CardImage'
 import ClientList from './components/ClientList'
-import { FeaturedAbout } from './components/FeaturedAbout'
+import FeaturedAbout from './components/FeaturedAbout'
 import MainVisual from './components/MainVisual'
 import Cta from './components/Cta'
 
