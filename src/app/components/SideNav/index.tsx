e992291c0d4fc0703eba4cb7711fa1228a867ba9
takahiro-okada <okahanako09875@gmail.com>
2024-03-19 06:32:40 +0900

    update: sidenaviの修正

--- src/app/components/SideNav/index.tsx
+++ src/app/components/SideNav/index.tsx
@@ -1,104 +1,89 @@
 'use client'
+import getAllCategories from '@/lib/queries/getAllCategories'
 import { useEffect, useState } from 'react'
+import { Category } from '@/lib/types'
 
-import type Categories from '../../types/categories'
+type SideNavProps = {
+  linkPrefix: string
+  categoryKey: string
+}
 
-export default function SideNav() {
-  const [categories, setCategories] = useState<Categories[] | null>(null)
-  useEffect(() => {
-    const query = `query GetCategories {
-      categories {
-        edges {
-          node {
-            id
-            name
-            slug
-            children {
-              edges {
-                node {
-                  id
-                  name
-                  slug
-                }
-              }
-            }
-          }
-        }
+const createHierarchy = (categories: Category[]) => {
+  const categoriesMap: { [key: string]: Category & { children: Category[] } } = {}
+  categories.forEach((category) => {
+    categoriesMap[category.slug] = { ...category, children: [] }
+  })
+  categories.forEach((category) => {
+    if (category.parent) {
+      if (categoriesMap[category.parent.node.slug]) {
+        categoriesMap[category.parent.node.slug].children.push(categoriesMap[category.slug])
       }
-    }`
+    }
+  })
 
-    fetch('https://wp.nebonga.com/graphql', {
-      body: JSON.stringify({ query }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        const extractedCategories = json.data.categories.edges.map((edge: any) => edge.node)
-        setCategories(extractedCategories)
-      })
-      .catch((error) => {
-        console.error('エラー:', error)
-      })
-  }, [])
+  const topLevelCategories: (Category & { children: Category[] })[] = Object.values(categoriesMap).filter(
+    (category) => !category.parent,
+  )
 
-  const renderCategory = (category: Categories) => (
-    <li key={category.id}>
-      <a href={`/works/${category.slug}`} className='inline-block w-full border-b-2 py-2 hover:underline'>
-        {category.name}
-      </a>
-      {category.children && category.children.edges.length > 0 && (
-        <ul className=''>
-          {category.children.edges.map((edge: any) => (
-            <li key={edge.node.id}>
-              <a
-                href={`/works/${category.slug}/${edge.node.slug}`}
-                className='inline-block w-full border-b-2 py-2 pl-4 hover:underline'
-              >
-                - {edge.node.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </li>
+  return topLevelCategories
+}
+
+export default function SideNav({ linkPrefix, categoryKey }: SideNavProps) {
+  const [categories, setCategories] = useState<(Category & { children: Category[] })[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result: Category[] = await getAllCategories(categoryKey)
+      console.log(result, 'result')
+      if (result) {
+        const hierarchy = createHierarchy(result)
+        setCategories(hierarchy)
+      }
+    }
+    fetchData()
+  }, [categoryKey])
+
+  const renderCategory = (category: Category & { children: Category[] }) => (
+    <>
+      <li key={category.slug}>
+        <a
+          href={`/${linkPrefix}/${category.slug}`}
+          className='inline-block w-full border-b-[1px] p-3 hover:bg-gray hover:rounded-md transition-all duration-700 ease-out'
+        >
+          {category.name}
+        </a>
+        {category.children && category.children.length > 0 && (
+          <ul>
+            {category.children.map((child) => (
+              <li key={child.slug}>
+                <a
+                  href={`/${linkPrefix}/${child.slug}`}
+                  className='inline-block w-full border-b-[1px] py-3 pl-8 hover:bg-gray hover:rounded-md transition-all duration-700 ease-out'
+                >
+                  - {child.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+    </>
   )
 
   return (
     <aside className='w-full'>
-      <p className='mb-4 w-full rounded-lg bg-slate-200 p-2 text-xl'>Category</p>
-      <ul className='space-y-4'>
-        {categories ? (
-          categories.map(
-            (category) => category.children && category.children.edges.length > 0 && renderCategory(category),
-          )
-        ) : (
-          <p>Loading categories...</p>
-        )}
+      <p className='mb-4 w-full rounded-lg bg-[#eee] p-2 text-xl'>Category</p>
+      <ul>
+        <li>
+          <a
+            href={`/${linkPrefix}`}
+            className='inline-block w-full border-b-[1px] p-3 hover:bg-gray hover:rounded-md transition-all duration-700 ease-out'
+          >
+            ALL
+          </a>
+        </li>
+        {categories.length > 0 ? categories.map(renderCategory) : <p>Loading categories...</p>}
       </ul>
-
-      <div className='mt-8'>
-        <p className='mb-4 w-full rounded-lg bg-slate-200 p-2 text-xl'>Recommended</p>
-        <ul className='space-y-4'>
-          <li key=''>
-            <a href='#' className='hover:underline'>
-              2023
-            </a>
-          </li>
-          <li key=''>
-            <a href='#' className='hover:underline'>
-              2022
-            </a>
-          </li>
-          <li key=''>
-            <a href='#' className='hover:underline'>
-              2021
-            </a>
-          </li>
-        </ul>
-      </div>
     </aside>
   )
 }
