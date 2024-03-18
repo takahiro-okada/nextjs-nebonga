963e2747780cf54ed5aff0bb36ba9cdcfd023615
takahiro-okada <okahanako09875@gmail.com>
2024-03-19 06:33:12 +0900

    update: 不要なファイルの削除

--- src/app/util/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Montserrat } from 'next/font/google'
-
-export const montserrat = Montserrat({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-})
--- src/app/util/formatDate.tsx
+++
@@ -1,8 +0,0 @@
-const formatDate = (dateString: string) => {
-  const options = { day: '2-digit' as const, month: '2-digit' as const, year: 'numeric' as const }
-  const date = new Date(dateString)
-  const formattedDate = new Intl.DateTimeFormat('ja-JP', options).format(date).replace(/\//g, '.')
-  return formattedDate
-}
-
-export default formatDate
