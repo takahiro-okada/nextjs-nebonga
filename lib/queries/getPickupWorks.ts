efb505b8df205bd1dbc12dd3c0ce4ea9d86ced7c
takahiro-okada <okahanako09875@gmail.com>
2024-04-01 23:40:35 +0900

    update: add getPcikupWorks.tsx

---
+++ lib/queries/getPickupWorks.ts
@@ -0,0 +1,42 @@
+import { fetchGraphQL } from '../functions'
+import { Post } from '../types'
+
+export default async function getPickupWorks(limit: number = 6) {
+  const query = `query GetPickupWorks{
+    works(
+      where: {taxQuery: {taxArray: {taxonomy: WORKSTAG, terms: "pickup", field: SLUG}}}
+    ) {
+      nodes {
+        databaseId
+        date
+        modified
+        title
+        slug
+        worksCategories {
+          nodes {
+            name
+            slug
+          }
+        }
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+            mediaDetails {
+                height
+                width
+            }
+          }
+        }
+      }
+    }
+  }`
+
+  const response = await fetchGraphQL(query, { limit })
+
+  const posts = response.data.works.nodes as Post[]
+
+  return {
+    posts,
+  }
+}
