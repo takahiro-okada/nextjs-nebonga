af56e1479b3913a01ecd234c985bc7d95c569a6a
takahiro-okada <okahanako09875@gmail.com>
2024-04-03 06:41:15 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import ClientList from './components/ClientList'
 import Cta from './components/Cta'
 import FeaturedAbout from './components/FeaturedAbout'
-import LatestWorks from './components/LatestWorks'
 import MainVisual from './components/MainVisual'
 
 export default function Home() {
