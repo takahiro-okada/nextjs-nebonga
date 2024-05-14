95e44f294a424b6a237a98d0f105535ac0b40f19
takahiro-okada <okahanako09875@gmail.com>
2024-05-14 18:22:04 +0900

    update

--- components/pages/AboutCompany/index.tsx
+++ components/pages/AboutCompany/index.tsx
@@ -18,7 +18,7 @@ export default function AboutCompany() {
               ease: 'easeOut',
             }}
           >
-            <h2 className='text-2xl font-bold tracking-tighter'>会社概要</h2>
+            <h2 className='text-2xl font-bold tracking-wide'>会社概要</h2>
           </motion.div>
           <motion.div
             viewport={{ once: true }}
@@ -32,54 +32,58 @@ export default function AboutCompany() {
           >
             <div className='grid-cols-2 items-start gap-4 md:grid md:gap-24'>
               <table className='text-left'>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>会社名</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>
-                    株式会社NeBonga(ネボンガ)
-                    <br />
-                    (英語表記:NeBonga Inc.)
-                  </td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>住所</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>
-                    〒1500043
-                    <br />
-                    東京都渋谷区道玄坂1丁目10番8号渋谷道玄坂東急ビル 2F-C
-                  </td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>代表</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>川崎 芳勲</td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>資本金</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>3,000,000円</td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>創業</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>2023年9月</td>
-                </tr>
+                <tbody>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>会社名</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>
+                      株式会社NeBonga(ネボンガ)
+                      <br />
+                      (英語表記:NeBonga Inc.)
+                    </td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>住所</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>
+                      〒1500043
+                      <br />
+                      東京都渋谷区道玄坂1丁目10番8号渋谷道玄坂東急ビル 2F-C
+                    </td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>代表</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>川崎 芳勲</td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>資本金</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>3,000,000円</td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>創業</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>2023年9月</td>
+                  </tr>
+                </tbody>
               </table>
               <table className='text-left'>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>事業内容</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>
-                    <ul className='list-disc'>
-                      <li>写真・動画・映画コンテンツの企画、撮影、編集業務及びその制作物の販売</li>
-                      <li>ソーシャルメディア、ＷＥＢサイト構築、運用並びにその他の広告宣伝</li>
-                      <li>各種セミナー、イベント等の企画、開催、運営等及び講演等の受託</li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>取引先銀行</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>三井住友銀行・GMOあおぞらネット銀行</td>
-                </tr>
-                <tr className='border-b border-grayNormal'>
-                  <th className='w-1/3 py-6 align-baseline md:w-1/5'>お問い合わせ</th>
-                  <td className='w-2/3 py-6 md:w-4/5'>contact@nebonga.com</td>
-                </tr>
+                <tbody>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>事業内容</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>
+                      <ul className='list-disc'>
+                        <li>写真・動画・映画コンテンツの企画、撮影、編集業務及びその制作物の販売</li>
+                        <li>ソーシャルメディア、ＷＥＢサイト構築、運用並びにその他の広告宣伝</li>
+                        <li>各種セミナー、イベント等の企画、開催、運営等及び講演等の受託</li>
+                      </ul>
+                    </td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>取引先銀行</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>三井住友銀行・GMOあおぞらネット銀行</td>
+                  </tr>
+                  <tr className='border-b border-grayNormal'>
+                    <th className='w-1/3 py-6 align-baseline md:w-1/5'>お問い合わせ</th>
+                    <td className='w-2/3 py-6 md:w-4/5'>contact@nebonga.com</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </motion.div>
