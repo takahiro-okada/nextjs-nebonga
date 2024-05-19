2fe8cd5e188b46400b753c0f7c681b790571a01a
takahiro-okada <okahanako09875@gmail.com>
2024-05-19 20:03:27 +0900

    update

---
+++ libs/queries/getTopWorks.ts
@@ -0,0 +1,74 @@
+import { fetchGraphQL } from '../functions'
+import { Post } from '../../typs/types'
+
+export default async function getSelectedWorks() {
+  const query = `query GetSelectedWorks {
+      page(id: "168", idType: DATABASE_ID) {
+        top_work_01 {
+          topWork01 {
+            ...WorkFields
+          }
+        }
+        top_work_02 {
+          topWork02 {
+            ...WorkFields
+          }
+        }
+        top_work_03 {
+          topWork03 {
+            ...WorkFields
+          }
+        }
+        top_work_04 {
+          topWork04 {
+            ...WorkFields
+          }
+        }
+        top_work_05 {
+          topWork05 {
+            ...WorkFields
+          }
+        }
+        top_work_06 {
+          topWork06 {
+            ...WorkFields
+          }
+        }
+      }
+    }
+    
+    fragment WorkFields on Work {
+      id
+      title
+      slug
+      databaseId
+      date
+      modified
+      featuredImage {
+        node {
+          altText
+          sourceUrl
+          mediaDetails {
+            sizes {
+              height
+              width
+            }
+          }
+        }
+      }
+    }`
+  const response = await fetchGraphQL(query)
+  const posts: Post[] = []
+  if (response.data && response.data.page) {
+    Object.keys(response.data.page).forEach((key) => {
+      const nestedKey = key.replace('top_work_', 'topWork') // This assumes keys like 'topWork01', 'topWork02', etc.
+      if (response.data.page[key] && response.data.page[key][nestedKey]) {
+        posts.push(response.data.page[key][nestedKey])
+      }
+    })
+  }
+
+  return {
+    posts,
+  }
+}
