51fdf9b8fcd0ae7c2020ae016b14ff1be2c8180e
takahiro-okada <okahanako09875@gmail.com>
2024-07-06 19:40:13 +0900

    fix: remove console.log

--- components/pages/MainVisual/index.tsx
+++ components/pages/MainVisual/index.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useRef, useState } from 'react'
 export default function MainVisual() {
   const [videoVisible, setVideoVisible] = useState(false)
   const videoRef = useRef<HTMLVideoElement | null>(null)
-  console.log('videoVisible', videoRef)
 
   useEffect(() => {
     if (videoVisible && videoRef.current) {
